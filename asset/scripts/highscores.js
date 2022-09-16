be3da3caccc8635a250d9ef2525d6aeebf9949ed
tuta-bits <tuta@angelic.com>
2022-09-17 04:16:44 +1000

    Modify add functions to highscores.js add stylings to highscores.css add elements to highscores.html

---
+++ asset/scripts/highscores.js
@@ -0,0 +1,8 @@
+const highScoresList = document.getElementById('highScoreList');
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+// PARSING HIGHSCORES FROM LOCAL STORAGE TO DISPLAY WITHIN <LI>
+//.map takes the set of arrays in highscores and convert it to whatver you're after
+highScoresList.innerHTML = highScores.map(score => {
+    return `<li class="high-score"> ${score.name} : ${score.score}</li>`
+}).join('');
